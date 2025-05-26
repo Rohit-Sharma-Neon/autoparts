a1648c276bd0d3a892c43efb6865e81fd1723335
Rohit Sharma <rohitsharmars0806@gmail.com>
2025-05-26 17:26:57 +0530

    Update app_strings.dart

--- lib/constant/app_strings.dart
+++ lib/constant/app_strings.dart
@@ -1,6 +1,6 @@
 class AppStrings {
   static const String appTitle = "Buy & Bee";
-  static const String kGoogleApiKey=  "AIzaSyArqlT_3Q9fHcisw6lvvUGTcObXGz3Gwer";
+  static const String kGoogleApiKey=  "***************";
   static const String notFoundUrl=  "https://assets5.lottiefiles.com/packages/lf20_no386ede.json";
   static const String notFoundAssets = "assets/no_data.json";
 
